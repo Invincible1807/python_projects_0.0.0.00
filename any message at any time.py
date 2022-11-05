14f0c41b1d9b9719a4ea68480b7f7201f0589c7d
Invincible1807 <90253415+Invincible1807@users.noreply.github.com>
2022-11-05 18:50:29 +0530

    Add files via upload

---
+++ any message at any time.py
@@ -0,0 +1,8 @@
+import pywhatkit as kit 
+
+number = '9825576427'
+
+kit.sendwhatmmsg(
+    number,
+    'kem cho maja ma',18,45
+)
---
+++ friend time pass.py
@@ -0,0 +1,7 @@
+import pyautogui as pg
+import time
+time.sleep(10)
+
+for i in range(1000):
+    pg.write("su chale be")
+    pg.pressI("Enter")
