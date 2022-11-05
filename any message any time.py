44089b752d71883dbbd19395fc1123f212df6e44
Invincible1807 <90253415+Invincible1807@users.noreply.github.com>
2022-11-05 18:44:43 +0530

    Add files via upload

---
+++ any message any time.py
@@ -0,0 +1,9 @@
+import pywhatkit as kit
+
+number = '+919825576427'
+
+kit.sendwhatmsg(
+    number,
+    'kem cho maja ma',18,55
+
+)
---
+++ friend disturb.py
@@ -0,0 +1,7 @@
+import pyautogui as pg
+import time
+time.sleep(10)
+
+for i in range(100):
+    pg.write("@yajur turn of your mic off ")
+    pg.press("Enter")
